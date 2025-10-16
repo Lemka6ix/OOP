60bf2437c6b79fe69bae8cb29038e91edc543fb8
Lemka6ix <klider.fire@mail.ru>
2025-10-16 14:38:15 +0500

    доделать

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include "point3d.h"
+#include "TorPointGen.h"
+#ifdef WITH_MATHGL
+#include <mg12/mgl.h>
+#endif
 
 int main() {
-    
+    double R, r;
+    std::cout << "Введите параметры тора (R и r): ";
+    std::cin >> R >> r;
+
+    TorPointGenerator generation(R, r);
+    generator.saveSettings();
 }
